6717d2bee474a51899d6895e96abf9e019632145
Marcos Mourão <dev@mourao.info>
2026-08-25 10:10:14 -0300

    chore: initialize React Vite project and clean boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<div>
+      <h1>Ticket Flow</h1>
+      <p>System initialized.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
